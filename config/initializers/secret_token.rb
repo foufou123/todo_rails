735833094d8dc3ee04f824d789a5fb1b9635936e
Jason Hamilton & Raouf Nabi <student@epicodus.com>
2014-03-31 12:28:32 -0700

    Initial Commit for Wiki Rails app Walk Through completed

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wikipages::Application.config.secret_key_base = 'a645ceb3607e9a4389aa764f85a4c7dd7ecc3b323a852e5ff4f26fa3c5f8baf390a9ec6e5ae5d9fbe34a396a87472d726db7b17619a0060ed70e6a2940f43384'
